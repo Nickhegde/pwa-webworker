aefc262237bd2b391c2eb20ffc5c02d6542cdca2
Nikhil Hegde <nikhil.hegde@ymedialabs.com>
2019-11-10 20:24:03 +0530

    Changes

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
         </header>
         <section className="App-intro">
           <Home />
